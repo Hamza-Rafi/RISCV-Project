121ebc9dec2de1cba891d3a6a9c960c7c0568b26
Hamza Rafi <rafi.hamza2020@gmail.com>
2025-01-16 15:58:18 +0000

    write changes to file

--- instructions/genfile.go
+++ instructions/genfile.go
@@ -78,11 +78,22 @@ func main() {
 	}
 	writer := bufio.NewWriter(newFile)
 
+	// write file Beginning
+	fileBeginning, err := os.ReadFile("fileBeginning.s")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Print(string(fileBeginning))
+	writer.Write(fileBeginning)
+	writer.Flush()
+
+	// loop 100 times
 	for count := 0; count < 100; count++ {
 		var registers = []string{"x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8", "x9", "x10", "x11", "x12", "x13", "x14", "x15", "x16", "x17", "x18", "x19", "x20", "x21", "x22", "x23", "x24", "x25", "x26", "x27", "x28", "x29", "x30"}
 		var r1, r2, r3, r4 string
 
-		writer.WriteString("-------------------" + strconv.Itoa(count) + "---------------------\n")
+		writer.WriteString("#-------------------" + strconv.Itoa(count) + "---------------------\n")
 
 		for i, section := range sections {
 
@@ -100,7 +111,7 @@ func main() {
 			for _, line := range section {
 				// if line is comment, ignore
 				if line[0] == '#' {
-					fmt.Println(line)
+					// fmt.Println(line)
 					writer.WriteString(line + "\n")
 					continue
 				}
@@ -123,6 +134,7 @@ func main() {
 					line = strings.Replace(line, "{instruction}", instruction, 1)
 					line = strings.Replace(line, "{r1}", r1, 1)
 					line = strings.Replace(line, "{r2}", r2, 1)
+					strings.repla
 
 					line = strings.Replace(line, "{r3}", getRandomRegister(registers, false), 1)
 
@@ -136,7 +148,7 @@ func main() {
 					line = strings.Replace(line, "{r2}", r2, 1)
 
 				}
-				fmt.Println(line)
+				// fmt.Println(line)
 
 				// write line to file
 				writer.WriteString(line + "\n")
@@ -144,9 +156,9 @@ func main() {
 			}
 
 		}
-		writer.WriteString("-----------------------------------------")
+		writer.WriteString("#-----------------------------------------\n\n")
 		writer.Flush()
 
-		fmt.Println(sections)
+		// fmt.Println(sections)
 	}
 }
